e83c4072a276c5277df39012e01ef6ea08270d29
Tejas Shah <tejasshah2k19@gmail.com>
2024-04-12 19:33:12 +0530

    spring.xml usercontroller eject

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,5 +1,7 @@
 package com;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.controller.UserController;
 
 /**
@@ -8,17 +10,31 @@ import com.controller.UserController;
  */
 public class App {
 	public static void main(String[] args) {
+//
+//		UserController uc = new UserController();
+//		uc.login();
+//		UserController uc1 = new UserController();
+//		uc1.login();
+//
+//		// 2 object uc uc1 -> 2 instance
+//
+//		//
+//		System.out.println(uc);// hashcode
+//		System.out.println(uc1);// hashcode
+//		
 
-		UserController uc = new UserController();
-		uc.login();
-		UserController uc1 = new UserController();
-		uc1.login();
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-		// 2 object uc uc1 -> 2 instance
+		UserController uc1 = context.getBean("userController", UserController.class);
+		UserController uc2 = context.getBean("userController", UserController.class);
+		UserController uc3 = context.getBean("userController", UserController.class);
+		UserController uc4 = context.getBean("userController", UserController.class);
 
-		//
-		System.out.println(uc);// hashcode
-		System.out.println(uc1);// hashcode
+		System.out.println(uc1);
+		System.out.println(uc2);
+		System.out.println(uc3);
+		System.out.println(uc4);
+		
 	}
 }
 //singleton 
